58588fd024c3a5fdf18c76c1568bf288facd8dd4
veeru <veerumedikonduru999@gmail.com>
2024-02-27 23:39:51 +0530

    intial comit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+function App() {
+  return (
+    <div className=' text-green-700'>App</div>
+  )
+}
+
+export default App
